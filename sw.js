c1b113cfeff828c36548e9bc025ec47f91949c21
khunryo <khunryo@gmail.com>
2026-08-01 06:05:01 +0900

    SW自動更新(controllerchange→reload・ガード)で旧キャッシュ固定を解消・県選択時のfit-to-bounds拡大・sw v29

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 釣り場マップ PWA service worker */
-const CACHE = 'fishing-map-v28';
+const CACHE = 'fishing-map-v29';
 const SHELL = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-512-maskable.png','./apple-touch-icon-180.png'];
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
